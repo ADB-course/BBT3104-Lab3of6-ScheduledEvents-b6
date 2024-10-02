6c6ee75f6f48cb591ef96b83d7a1a746fc9dad19
Alfred76-web <alfred.njaku@strathmore.edu>
2024-10-02 10:40:11 +0300

    #lab_submissioon

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,9 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+CREATE TABLE EVN_average_customer_waiting_time_every_1_hour (
+    entity_id INT PRIMARY KEY, 
+    timestamp TIMESTAMP NOT NULL, 
+    average_waiting_time DECIMAL(10, 2) NOT NULL 
+);
+
+INSERT INTO EVN_average_customer_waiting_time_every_1_hour (entity_id, timestamp, average_waiting_time)
+VALUES (1, '2024-09-28 10:00:00', 0.25); -- Average waiting time of 15 minutes for entity 1 at 10 AM
